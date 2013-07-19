03937bb6885a245e1ca5af14cc39f7249ad8de5e
Timothy Washington <twashing@gmail.com>
2013-07-19 16:19:34 -0700

    -- removing quux.core from test ns requires -- adding 'changes' file to .gitignore -- adding some test directories and sample files

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject stefon "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A composable blogging engine in Clojure."
+  :url "https://github.com/twashing/stefon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+
+  )
