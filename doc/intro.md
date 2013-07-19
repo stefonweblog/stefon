69d1c7a88bb023a1712ad88d8410480ca4917456
Timothy Washington <twashing@gmail.com>
2013-07-19 07:45:04 -0700

    -- first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stefon
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/stefon/core_test.clj
@@ -0,0 +1,7 @@
+(ns stefon.core-test
+  (:require [clojure.test :refer :all]
+            [stefon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
