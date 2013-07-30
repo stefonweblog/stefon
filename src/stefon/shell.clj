227fc8a780247d81beacd3a4b5f43332125189fd
Timothy Washington <twashing@gmail.com>
2013-07-29 17:52:04 -0700

    -- doing some spacing clean-up

--- src/stefon/shell.clj
+++ src/stefon/shell.clj
@@ -6,8 +6,8 @@
 
 ;; ====
 ;; SYSTEM structure & functions
-(def ^{:doc "In memory representation of the running system structures"}
-  ^:dynamic *SYSTEM* (atom nil))
+(def  ^{:doc "In memory representation of the running system structures"}
+      ^:dynamic *SYSTEM* (atom nil))
 
 (defn create-system []
 
@@ -20,8 +20,7 @@
 
 (defn start-system
 
-  ([]
-     (start-system (create-system)))
+  ([] (start-system (create-system)))
 
   ([system]
 
@@ -41,60 +40,29 @@
 
 
 ;; Posts
-(defn create-post [title content created-date]
-  (functions/create *SYSTEM* :posts 'stefon.domain.Post title content created-date))
-
-(defn retrieve-post [ID]
-  (functions/retrieve *SYSTEM* :posts ID))
-
-(defn update-post [ID update-map]
-  (functions/update *SYSTEM* :posts ID update-map))
-
-(defn delete-post [ID]
-  (functions/delete *SYSTEM* :posts ID))
-
-(defn find-posts [param-map]
-  (functions/find *SYSTEM* :posts param-map))
-
-(defn list-posts []
-  (functions/list *SYSTEM* :posts))
+(defn create-post [title content created-date] (functions/create *SYSTEM* :posts 'stefon.domain.Post title content created-date))
+(defn retrieve-post [ID] (functions/retrieve *SYSTEM* :posts ID))
+(defn update-post [ID update-map] (functions/update *SYSTEM* :posts ID update-map))
+(defn delete-post [ID] (functions/delete *SYSTEM* :posts ID))
+(defn find-posts [param-map] (functions/find *SYSTEM* :posts param-map))
+(defn list-posts [] (functions/list *SYSTEM* :posts))
 
 
 ;; Assets
-(defn create-asset [asset type]
-  (functions/create *SYSTEM* :assets 'stefon.domain.Asset asset type))
-
-(defn retrieve-asset [ID]
-  (functions/retrieve *SYSTEM* :assets ID))
-
-(defn update-asset [ID update-map]
-  (functions/update *SYSTEM* :assets ID update-map))
-
-(defn delete-asset [ID]
-  (functions/delete *SYSTEM* :assets ID))
-
-(defn find-assets [param-map]
-  (functions/find *SYSTEM* :assets param-map))
-
-(defn list-assets []
-  (functions/list *SYSTEM* :assets))
+(defn create-asset [asset type] (functions/create *SYSTEM* :assets 'stefon.domain.Asset asset type))
+(defn retrieve-asset [ID] (functions/retrieve *SYSTEM* :assets ID))
+(defn update-asset [ID update-map] (functions/update *SYSTEM* :assets ID update-map))
+(defn delete-asset [ID] (functions/delete *SYSTEM* :assets ID))
+(defn find-assets [param-map] (functions/find *SYSTEM* :assets param-map))
+(defn list-assets [] (functions/list *SYSTEM* :assets))
 
 
 ;; Tags
-(defn create-tag [name]
-  (functions/create *SYSTEM* :tags 'stefon.domain.Tag name))
+(defn create-tag [name] (functions/create *SYSTEM* :tags 'stefon.domain.Tag name))
+(defn retrieve-tag [ID] (functions/retrieve *SYSTEM* :tags ID))
+(defn update-tag [ID update-map] (functions/update *SYSTEM* :tags ID update-map))
+(defn delete-tag [ID] (functions/delete *SYSTEM* :tags ID))
+(defn find-tags [param-map] (functions/find *SYSTEM* :tags param-map))
+(defn list-tags [] (functions/list *SYSTEM* :tags))
 
-(defn retrieve-tag [ID]
-  (functions/retrieve *SYSTEM* :tags ID))
 
-(defn update-tag [ID update-map]
-  (functions/update *SYSTEM* :tags ID update-map))
-
-(defn delete-tag [ID]
-  (functions/delete *SYSTEM* :tags ID))
-
-(defn find-tags [param-map]
-  (functions/find *SYSTEM* :tags param-map))
-
-(defn list-tags []
-  (functions/list *SYSTEM* :tags))
