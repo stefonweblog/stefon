9fa1229e2a248e67a2c30e448d176b32261c870d
Timothy Washington <twashing@gmail.com>
2013-08-05 18:34:15 -0700

    -- incl. Stefon-datomic in stefon

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
                  [cljs-uuid "0.0.4"]
                  [lamina "0.5.0-rc3"]
                  [com.datomic/datomic "0.8.3335"
-                  :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]]
+                  :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
+
+                 ;; Plugins
+                 [stefon-datomic "0.1.0-SNAPSHOT"]]
 
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
