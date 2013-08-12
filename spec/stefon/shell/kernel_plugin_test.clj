6fccabeac8f3fc2ac78fa2c24cc699c04d9a4409
Timothy Washington <twashing@gmail.com>
2013-08-12 10:44:15 -0700

    -- finished converting 'kernel_plugin_test.clj' to speclj

---
+++ spec/stefon/shell/kernel_plugin_test.clj
@@ -0,0 +1,80 @@
+(ns stefon.shell.kernel-plugin-test
+
+  (:require [speclj.core :refer :all]
+            [lamina.core :as lamina]
+
+            [stefon.shell :as shell]
+            [stefon.shell.plugin :as plugin]
+            [stefon.shell.kernel :as kernel]))
+
+(describe "one"
+
+          (it "Test kernel receive from plugin message"
+
+              (let [system (plugin/create-plugin-system (shell/create-system))
+                    handler (fn [inp] inp)
+                    sender (plugin/attach-plugin system handler)
+
+                    result-event (atom nil)
+                    kernel-handler (fn [inp] (swap! result-event (fn [i] inp)))
+
+                    xxx (kernel/attach-kernel system kernel-handler)
+                    xxy (sender {:fu :bar :qwerty :board})]
+
+                (should-not-be-nil @result-event)
+
+                (should= clojure.lang.PersistentArrayMap (type @result-event))
+                (should-contain :fu (keys @result-event))
+                (should-contain :qwerty (keys @result-event))))
+
+
+          (it "Test attaching kernel to the shell"
+
+                (let [system (shell/create-system)
+                      result-event (atom nil)
+                      kernel-handler (fn [inp] (swap! result-event (fn [i] inp)))
+
+                      system-with-handler (shell/start-system system kernel-handler)
+
+                      ;; something needs to send an event
+                      handler (fn [inp] inp)
+                      sender (plugin/attach-plugin @system-with-handler handler)
+
+                      result-send (sender {:fu :bar :qwerty :board})]
+
+                  (should-not-be-nil @result-event)
+                  (should= clojure.lang.PersistentArrayMap (type @result-event))
+                  (should-contain :fu (keys @result-event))
+                  (should-contain :qwerty (keys @result-event))))
+
+
+          (it "Test kernel action map from plugin message"
+
+              (let [system (shell/create-system)
+                    system-with-handler (shell/start-system system)
+
+                    ;; something needs to send an event
+                    handler (fn [inp]
+
+                              (println (str "test plugin handler [" inp "]")))
+                    sender (plugin/attach-plugin @system-with-handler handler)
+
+                    result-send (sender {:stefon.post.create {:parameters {:title "Latest In Biotech" :content "Lorem ipsum." :created-date "0000"}}
+                                         :fu :bar})])
+
+              (should true)
+              ;; TODO - so far just testing with println ; find a better way to test kernel handler
+
+              ;; did handler receive event
+
+              ;; did correct function get triggered, from a mapped action
+              ;; did the triggered function, send out a message to other plugins after the fact
+
+              ;; if applicable, did multiple functions get triggred
+
+              ;; did the kernel ignore actions it did not recognize
+              ;; did the kernel forward on, to other plugins, the actions it did not recognize
+
+
+              ;; howto test how many times a function was called
+              ))
