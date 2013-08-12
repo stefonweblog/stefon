ce76e9d316658c214218ccf5c1154a0f929f83a9
Timothy Washington <twashing@gmail.com>
2013-08-12 11:27:34 -0700

    -- finished porting over 'plugin_test.clj' to speclj

--- spec/stefon/shell/plugin_test.clj
+++ spec/stefon/shell/plugin_test.clj
@@ -33,59 +33,63 @@
 
 
           ;; ====
-          #_(let [handler-fn (fn [event])
-                sender-fn (plugin/attach-plugin @(kernel/get-system) handler-fn)]
+          (it "Ensure we're getting back a sender function"
 
-            #_(fact "Ensure we're getting back a sender function"
+             (let [handler-fn (fn [event])
+                   sender-fn (plugin/attach-plugin @(kernel/get-system) handler-fn)]
 
-                  sender-fn =not=> nil?
-                  sender-fn => fn?))
+               (should-not-be-nil sender-fn)
+               (should (fn? sender-fn))))
 
-          #_(let [result-event (atom nil)
-                handler-fn (fn [event] (swap! result-event (fn [i] event)))
-                sender-fn (plugin/attach-plugin @(kernel/get-system) handler-fn)]
 
-            #_(fact "Ensure that result begins as empty" @result-event => nil?)
-            #_(fact "Ensure that kernel sending messages is recieved by handler function"
+
+          (it "Ensure that result begins as empty, and then occupied after a message is sent"
+
+              (let [result-event (atom nil)
+                    handler-fn (fn [event] (swap! result-event (fn [i] event)))
+                    sender-fn (plugin/attach-plugin @(kernel/get-system) handler-fn)]
+
+                ;; result-event should initially be empty
+                (should-be-nil @result-event)
+
+                ;; publish a message
+                (plugin/publish-event @(kernel/get-system) {:fu :bar})
+
+                ;; now should have content
+                (should-not-be-nil @result-event)
+                (should-contain :fu (keys @result-event))))
+
+
+          ;; TODO - howto test that a function was invoked multiple times
+          #_(it "Test for multiple sends from kernel"
+
+                (let [handler-1 (fn [inp] inp)
+                      sender-1 (plugin/attach-plugin @(kernel/get-system) handler-1)
+                      call-multiple (fn []
+
+                                      (plugin/publish-event @(kernel/get-system) {:fu :bar}) => nil?
+                                      (plugin/publish-event @(kernel/get-system) {:interrupt :software}) => nil?
+                                      nil)]
+
+                  (call-multiple) => nil?
+                  (provided
+                   (handler-1 {}) => {} :times 2)))
+
+          (it "Test for multiple handlers receiving a send"
+
+                (let [r1 (atom nil)
+                      r2 (atom nil)
+
+                      h1 (fn [inp] (swap! r1 (fn [i] inp)))
+                      h2 (fn [inp] (swap! r2 (fn [i] inp)))
+
+                      sender-1 (plugin/attach-plugin @(kernel/get-system) h1)
+                      sender-2 (plugin/attach-plugin @(kernel/get-system) h2)]
 
                   (plugin/publish-event @(kernel/get-system) {:fu :bar})
 
-                  @result-event =not=> nil?
-                  (keys @result-event)  => (contains #{:fu}))))
+                  (should-not-be-nil @r1)
+                  (should-not-be-nil @r2)
 
-
-#_(against-background [(before :facts (shell/start-system))
-                     (after :facts (shell/stop-system))]
-
-                    (fact "Test for multiple sends from kernel"
-
-                            (let [handler-1 (fn [inp] inp)
-                                  sender-1 (plugin/attach-plugin @(kernel/get-system) handler-1)
-                                  call-multiple (fn []
-
-                                                  (plugin/publish-event @(kernel/get-system) {:fu :bar}) => nil?
-                                                  (plugin/publish-event @(kernel/get-system) {:interrupt :software}) => nil?
-                                                  nil)]
-
-                              (call-multiple) => nil?
-                              (provided
-                               (handler-1 {}) => {} :times 2)))
-
-                    (fact "Test for multiple handlers receiving a send"
-
-                          (let [r1 (atom nil)
-                                r2 (atom nil)
-
-                                h1 (fn [inp] (swap! r1 (fn [i] inp)))
-                                h2 (fn [inp] (swap! r2 (fn [i] inp)))
-
-                                sender-1 (plugin/attach-plugin @(kernel/get-system) h1)
-                                sender-2 (plugin/attach-plugin @(kernel/get-system) h2)]
-
-                            (plugin/publish-event (kernel/get-system) {:fu :bar})
-
-                            @r1 =not=> nil?
-                            @r2 =not=> nil?
-
-                            (keys @r1) => (contains #{:fu})
-                            (keys @r2) => (contains #{:fu}))))
+                  (should-contain :fu (keys @r1))
+                  (should-contain :fu (keys @r2)))))
