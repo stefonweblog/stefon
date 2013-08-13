1f9ebe1df8275fc663aa210fbe61927ca0c495e3
Timothy Washington <twashing@gmail.com>
2013-08-13 14:13:56 -0700

    -- altering other domain tests to use new schema-generated approach

--- spec/stefon/domain_test.clj
+++ spec/stefon/domain_test.clj
@@ -11,23 +11,21 @@
               (let [post-type (domain/gen-post-type)
                     uuidS (str (uuid/make-random))]
 
-
-                (println "> Post type[" (type post-type) "] > Post[" post-type "]")
-
                 (should-not-be-nil post-type)
                 (should= {:id uuidS :title "ta" :content "ca" :content-type "c/t" :created-date "07202013" :modified-date "07222013"}
                          (into {} (stefon.domain.Post. uuidS "ta" "ca" "c/t" "07202013" "07222013")))))
 
-          #_(it "Generating an Asset type"
+          (it "Generating an Asset type"
 
               (let [asset-type (domain/gen-asset-type)
                     uuidS (str (uuid/make-random))]
 
                 (should-not-be-nil asset-type)
-                (should= {:id uuidS :asset "binary-content" :type "image"} (into {} (stefon.domain.Asset. uuidS "binary-content" "image")))))
+                (should= {:id uuidS :name "myimage" :type "image" :asset "binary-content"}
+                         (into {} (stefon.domain.Asset. uuidS "myimage" "image" "binary-content")))))
 
 
-          #_(it "Generating a Tag type"
+          (it "Generating a Tag type"
 
               (let [tag-type (domain/gen-tag-type)
                     uuidS (str (uuid/make-random))]
