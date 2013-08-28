643b29f89fb1e40aff6de9f859a47c468fb5eb59
Timothy Washington <twashing@gmail.com>
2013-08-28 12:20:09 -0700

    -- making shell functions pass through kernel/handle-incoming-messages; this funnels actions through plugin pipeline channel

--- src/stefon/shell.clj
+++ src/stefon/shell.clj
@@ -63,47 +63,76 @@
 
 
 ;; CRUD Wrappers around kernel functions
+;; CREATE
 (defmulti create (fn [& arg-list] (first arg-list)))
-(defmethod create :post ([& arg-list]
-                           (let [args (rest arg-list)]
-                             (kernel/create-post (nth args 0)
-                                                 (nth args 1)
-                                                 (nth args 2)
-                                                 (nth args 3)
-                                                 (nth args 4)))))
-(defmethod create :asset ([& arg-list]
-                            (let [args (rest arg-list)]
-                              (kernel/create-asset (nth args 0)
-                                                   (nth args 1)
-                                                   (nth args 2)))))
-(defmethod create :tag ([& arg-list] (let [args (rest arg-list)] (kernel/create-tag (first args) ))))
+(defmethod create :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/handle-incoming-messages {:stefon.post.create {:parameters {:title (nth args 0)
+                                                                                                            :content (nth args 1)
+                                                                                                            :content-type (nth args 2)
+                                                                                                            :created-date (nth args 3)
+                                                                                                            :modified-date (nth args 4)}}}))))
+(defmethod create :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/handle-incoming-messages {:stefon.asset.create {:parameters {:name (nth args 0)
+                                                                                                              :type (nth args 1)
+                                                                                                              :asset (nth args 2)}}}))))
 
+(defmethod create :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/handle-incoming-messages {:stefon.tag.create {:parameters {:name (nth args 0)}}}))))
 
+;; RETRIEVE
 (defmulti retrieve (fn [& arg-list] (first arg-list)))
-(defmethod retrieve :post ([& arg-list] (let [args (rest arg-list)] (kernel/retrieve-post (first args) ))))
-(defmethod retrieve :asset ([& arg-list] (let [args (rest arg-list)] (kernel/retrieve-asset (first args)))))
-(defmethod retrieve :tag ([& arg-list] (let [args (rest arg-list)] (kernel/retrieve-tag (first args) ))))
+(defmethod retrieve :post ([& arg-list] (let [args (rest arg-list)]
+                                          (kernel/handle-incoming-messages {:stefon.post.retrieve {:parameters {:id (nth args 0)}}}))))
 
+(defmethod retrieve :asset ([& arg-list] (let [args (rest arg-list)]
+                                           (kernel/handle-incoming-messages {:stefon.asset.retrieve {:parameters {:name (nth args 0)}}}))))
 
+(defmethod retrieve :tag ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/handle-incoming-messages {:stefon.tag.retrieve {:parameters {:name (nth args 0)}}}))))
+
+;; UPDATE
 (defmulti update (fn [& arg-list] (first arg-list)))
-(defmethod update :post ([& arg-list] (let [args (rest arg-list)] (kernel/update-post (first args) (second args)))))
-(defmethod update :asset ([& arg-list] (let [args (rest arg-list)] (kernel/update-asset (first args) (second args)))))
-(defmethod update :tag ([& arg-list] (let [args (rest arg-list)] (kernel/update-tag (first args) (second args)))))
+(defmethod update :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/handle-incoming-messages {:stefon.post.update {:parameters {:id (nth args 0)
+                                                                                                            :update-map (nth args 1)}}}))))
 
+(defmethod update :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/handle-incoming-messages {:stefon.asset.update {:parameters {:id (nth args 0)
+                                                                                                              :update-map (nth args 1)}}}))))
 
+(defmethod update :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/handle-incoming-messages {:stefon.tag.update {:parameters {:id (nth args 0)
+                                                                                                          :update-map (nth args 1)}}}))))
+
+;; DELETE
 (defmulti delete (fn [& arg-list] (first arg-list)))
-(defmethod delete :post ([& arg-list] (let [args (rest arg-list)] (kernel/delete-post (first args) ))))
-(defmethod delete :asset ([& arg-list] (let [args (rest arg-list)] (kernel/delete-asset (first args) ))))
-(defmethod delete :tag ([& arg-list] (let [args (rest arg-list)] (kernel/delete-tag (first args) ))))
+(defmethod delete :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/handle-incoming-messages {:stefon.post.delete {:parameters {:id (nth args 0)}}}))))
 
+(defmethod delete :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/handle-incoming-messages {:stefon.asset.delete {:parameters {:id (nth args 0)}}}))))
 
+(defmethod delete :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/handle-incoming-messages {:stefon.tag.delete {:parameters {:id (nth args 0)}}}))))
+
+;; FIND
 (defmulti find (fn [& arg-list] (first arg-list)))
-(defmethod find :post ([& arg-list] (let [args (rest arg-list)] (kernel/find-posts (first args)))))
-(defmethod find :asset ([& arg-list] (let [args (rest arg-list)] (kernel/find-assets (first args) ))))
-(defmethod find :tag ([& arg-list] (let [args (rest arg-list)] (kernel/find-tags (first args) ))))
+(defmethod find :post ([& arg-list] (let [args (rest arg-list)]
+                                      (kernel/handle-incoming-messages {:stefon.post.find {:parameters {:param-map (nth args 0)}}}))))
 
+(defmethod find :asset ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/handle-incoming-messages {:stefon.asset.find {:parameters {:param-map (nth args 0)}}}))))
 
+(defmethod find :tag ([& arg-list] (let [args (rest arg-list)]
+                                     (kernel/handle-incoming-messages {:stefon.tag.find {:parameters {:param-map (nth args 0)}}}))))
+
+;; LIST
 (defmulti list (fn [& arg-list] (first arg-list)))
-(defmethod list :post ([& arg-list] (let [args (rest arg-list)] (kernel/list-posts))))
-(defmethod list :asset ([& arg-list] (let [args (rest arg-list)] (kernel/list-assets))))
-(defmethod list :tag ([& arg-list] (let [args (rest arg-list)] (kernel/list-tags))))
+(defmethod list :post ([& arg-list] (let [args (rest arg-list)]
+                                      (kernel/handle-incoming-messages {:stefon.post.list {:parameters nil}}))))
+
+(defmethod list :asset ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/handle-incoming-messages {:stefon.asset.list {:parameters nil}}))))
+
+(defmethod list :tag ([& arg-list] (let [args (rest arg-list)]
+                                     (kernel/handle-incoming-messages {:stefon.tag.list {:parameters nil}}))))
