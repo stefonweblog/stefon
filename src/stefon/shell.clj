18ba7fea7c36c27ca584af5583dec8e6fd027986
Timothy Washington <twashing@gmail.com>
2013-08-28 11:09:16 -0700

    -- adding wrapper functions CRUD functions in the shell

--- src/stefon/shell.clj
+++ src/stefon/shell.clj
@@ -59,3 +59,62 @@
 
 (defn- publish-event [^clojure.lang.PersistentHashMap event]
   (plugin/publish-event @(kernel/get-system) event))
+
+
+
+(defmulti create (fn [& arg-list] (first arg-list)))
+(defmethod create :post ([& arg-list] ))
+(defmethod create :asset ([& arg-list] ))
+(defmethod create :tag ([& arg-list] ))
+
+(defmulti retrieve (fn [& arg-list] (first arg-list)))
+(defmethod retrieve :post ([& arg-list] ))
+(defmethod retrieve :asset ([& arg-list] ))
+(defmethod retrieve :tag ([& arg-list] ))
+
+(defmulti update (fn [& arg-list] (first arg-list)))
+(defmethod update :post ([& arg-list] ))
+(defmethod update :asset ([& arg-list] ))
+(defmethod update :tag ([& arg-list] ))
+
+(defmulti delete (fn [& arg-list] (first arg-list)))
+(defmethod delete :post ([& arg-list] ))
+(defmethod delete :asset ([& arg-list] ))
+(defmethod delete :tag ([& arg-list] ))
+
+(defmulti find (fn [& arg-list] (first arg-list)))
+(defmethod find :post ([& arg-list] ))
+(defmethod find :asset ([& arg-list] ))
+(defmethod find :tag ([& arg-list] ))
+
+(defmulti list (fn [& arg-list] (first arg-list)))
+(defmethod list :post ([& arg-list] ))
+(defmethod list :asset ([& arg-list] ))
+(defmethod list :tag ([& arg-list] ))
+
+
+;; Posts
+(defn create-post [title content content-type created-date modified-date] )
+(defn retrieve-post [ID] )
+(defn update-post [ID update-map] )
+(defn delete-post [ID] )
+(defn find-posts [param-map] )
+(defn list-posts [] )
+
+
+;; Assets
+(defn create-asset [name type asset] )
+(defn retrieve-asset [ID] )
+(defn update-asset [ID update-map] )
+(defn delete-asset [ID] )
+(defn find-assets [param-map] )
+(defn list-assets [] )
+
+
+;; Tags
+(defn create-tag [name] )
+(defn retrieve-tag [ID] )
+(defn update-tag [ID update-map] )
+(defn delete-tag [ID] )
+(defn find-tags [param-map] )
+(defn list-tags [] )
