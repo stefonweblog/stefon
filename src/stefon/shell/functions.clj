18e2ef64c2013ee230be1a53c3e8254d0aafdb5f
Timothy Washington <twashing@gmail.com>
2013-09-28 05:02:42 -0700

    -- updated domain functions to account for new domain structure

--- src/stefon/shell/functions.clj
+++ src/stefon/shell/functions.clj
@@ -13,31 +13,31 @@
   (let [uuidS (str (uuid/make-random))
         entity-record (eval `(new ~klass ~uuidS ~@args))]
 
-    (swap! system-atom update-in [:domain dkey] conj entity-record)
+    (swap! system-atom update-in [:system :domain dkey] conj entity-record)
     entity-record))
 
 (defn retrieve [system-atom dkey ID]
 
   (first
-   (filter #(= (:id %) ID) (-> @system-atom :domain dkey))))
+   (filter #(= (:id %) ID) (-> @system-atom :system :domain dkey))))
 
 (defn update [system-atom dkey ID update-map]
 
   {:pre (map? update-map)}
 
   (let [indexed-entry (seq (first (filter #(= (-> % second :id) ID)
-                                          (map-indexed (fn [idx itm] [idx itm]) (-> @system-atom :domain dkey)))))]
+                                          (map-indexed (fn [idx itm] [idx itm]) (-> @system-atom :system :domain dkey)))))]
 
     (swap! system-atom
            update-in
-           [:domain dkey (first indexed-entry)]
+           [:system :domain dkey (first indexed-entry)]
            (fn [inp]
 
              (into inp update-map)))))
 
 (defn delete [system-atom dkey ID]
 
-  (swap! system-atom update-in [:domain dkey] (fn [inp]
+  (swap! system-atom update-in [:system :domain dkey] (fn [inp]
                                        (remove #(= (:id %) ID) inp))))
 
 (defn find
@@ -46,8 +46,8 @@
 
   (let [entries (seq param-map)]
 
-    (seq (set/join [param-map] (-> @system-atom :domain dkey)))))
+    (seq (set/join [param-map] (-> @system-atom :system :domain dkey)))))
 
 (defn list [system-atom dkey]
 
-  (-> @system-atom :domain dkey))
+  (-> @system-atom :system :domain dkey))
