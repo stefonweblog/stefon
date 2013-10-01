df471662813ad469d92f691151ea6e1f399f2008
Timothy Washington <twashing@gmail.com>
2013-09-30 17:55:00 -0700

    -- adding back CRUD functions to shell ns

--- src/stefon/shell.clj
+++ src/stefon/shell.clj
@@ -20,3 +20,83 @@
 
 (defn attach-plugin [handlerfn]
   (plugin/attach-plugin kernel/*SYSTEM* handlerfn))
+
+
+
+;; CRUD Wrappers around kernel functions
+;; CREATE
+(defmulti create (fn [& arg-list] (first arg-list)))
+(defmethod create :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/kernel-handler  {:message {:stefon.post.create {:parameters {:title (nth args 0)
+                                                                                                             :content (nth args 1)
+                                                                                                             :content-type (nth args 2)
+                                                                                                             :created-date (nth args 3)
+                                                                                                             :modified-date (nth args 4)
+                                                                                                             :assets (nth args 5)
+                                                                                                             :tags (nth args 6)}}}}))))
+
+(defmethod create :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/kernel-handler {:message {:stefon.asset.create {:parameters {:name (nth args 0)
+                                                                                                                 :type (nth args 1)
+                                                                                                                 :asset (nth args 2)}}}}))))
+
+(defmethod create :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/kernel-handler {:message {:stefon.tag.create {:parameters {:name (nth args 0)}}}}))))
+
+;; RETRIEVE
+(defmulti retrieve (fn [& arg-list] (first arg-list)))
+(defmethod retrieve :post ([& arg-list] (let [args (rest arg-list)]
+                                          (kernel/kernel-handler {:message {:stefon.post.retrieve {:parameters {:id (nth args 0)}}}}))))
+
+(defmethod retrieve :asset ([& arg-list] (let [args (rest arg-list)]
+                                           (kernel/kernel-handler {:message {:stefon.asset.retrieve {:parameters {:name (nth args 0)}}}}))))
+
+(defmethod retrieve :tag ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/kernel-handler {:message {:stefon.tag.retrieve {:parameters {:name (nth args 0)}}}}))))
+
+;; UPDATE
+(defmulti update (fn [& arg-list] (first arg-list)))
+(defmethod update :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/kernel-handler {:message {:stefon.post.update {:parameters {:id (nth args 0)
+                                                                                                               :update-map (nth args 1)}}}}))))
+
+(defmethod update :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/kernel-handler {:message {:stefon.asset.update {:parameters {:id (nth args 0)
+                                                                                                                 :update-map (nth args 1)}}}}))))
+
+(defmethod update :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/kernel-handler {:message {:stefon.tag.update {:parameters {:id (nth args 0)
+                                                                                                             :update-map (nth args 1)}}}}))))
+
+;; DELETE
+(defmulti delete (fn [& arg-list] (first arg-list)))
+(defmethod delete :post ([& arg-list] (let [args (rest arg-list)]
+                                        (kernel/kernel-handler {:message {:stefon.post.delete {:parameters {:id (nth args 0)}}}}))))
+
+(defmethod delete :asset ([& arg-list] (let [args (rest arg-list)]
+                                         (kernel/kernel-handler {:message {:stefon.asset.delete {:parameters {:id (nth args 0)}}}}))))
+
+(defmethod delete :tag ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/kernel-handler {:message {:stefon.tag.delete {:parameters {:id (nth args 0)}}}}))))
+
+;; FIND
+(defmulti find (fn [& arg-list] (first arg-list)))
+(defmethod find :post ([& arg-list] (let [args (rest arg-list)]
+                                      (kernel/kernel-handler {:message {:stefon.post.find {:parameters {:param-map (nth args 0)}}}}))))
+
+(defmethod find :asset ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/kernel-handler {:message {:stefon.asset.find {:parameters {:param-map (nth args 0)}}}}))))
+
+(defmethod find :tag ([& arg-list] (let [args (rest arg-list)]
+                                     (kernel/kernel-handler {:message {:stefon.tag.find {:parameters {:param-map (nth args 0)}}}}))))
+
+;; LIST
+(defmulti list (fn [& arg-list] (first arg-list)))
+(defmethod list :post ([& arg-list] (let [args (rest arg-list)]
+                                      (kernel/kernel-handler {:message {:stefon.post.list {:parameters nil}}}))))
+
+(defmethod list :asset ([& arg-list] (let [args (rest arg-list)]
+                                       (kernel/kernel-handler {:message {:stefon.asset.list {:parameters nil}}}))))
+
+(defmethod list :tag ([& arg-list] (let [args (rest arg-list)]
+                                     (kernel/kernel-handler {:message {:stefon.tag.list {:parameters nil}}}))))
